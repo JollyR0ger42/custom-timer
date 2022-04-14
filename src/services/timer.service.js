f83d04ce03b91127096bb807e9cb64bc0603d8da
JollyR0ger <kav993@gmail.com>
2022-04-14 14:36:45 +0300

    add Auth headers to timer service

--- src/services/timer.service.js
+++ src/services/timer.service.js
@@ -1,9 +1,13 @@
 const SERVER = process.env.VUE_APP_SERVER
+import {authHeader} from './auth-header.js'
 
 const newTimer = async (newTimer) => {
   const response = await fetch(SERVER + 'timers', {
     method: 'POST',
-    headers: {'Content-Type': 'application/json'},
+    headers: {
+      'Content-Type': 'application/json',
+      ...authHeader()
+    },
     body: JSON.stringify(newTimer)
   })
   const timers = await response.json()
@@ -11,13 +15,16 @@ const newTimer = async (newTimer) => {
 }
 
 const getTimers = async () => {
-  const response = await fetch(SERVER + 'timers')
+  const response = await fetch(SERVER + 'timers', {headers: authHeader()})
   const timers = await response.json()
   return timers
 }
 
 const removeTimerById = async (id) => {
-  const response = await fetch(SERVER + 'timers/' + id, {method: 'DELETE'})
+  const response = await fetch(SERVER + 'timers/' + id, {
+    method: 'DELETE',
+    headers: authHeader()
+  })
   const timers = await response.json()
   return timers
 }
@@ -25,7 +32,10 @@ const removeTimerById = async (id) => {
 const updTimerById = async (id, updFields) => {
   const response = await fetch(SERVER + 'timers/' + id, {
     method: 'PUT',
-    headers: {'Content-Type': 'application/json'},
+    headers: {
+      'Content-Type': 'application/json',
+      ...authHeader()
+    },
     body: JSON.stringify(updFields)
   })
   const timer = await response.json()
