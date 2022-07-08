7a5fd22212e61ff841367c51f15335e81a106803
JollyR0ger <kav993@gmail.com>
2022-07-08 13:55:58 +0300

    add notification toggle

--- src/layers/AppSettings.vue
+++ src/layers/AppSettings.vue
@@ -1,11 +1,45 @@
 <template>
-  <div>
-    settings
+  <div class="app-settings">
+    <h4  class="app-settings__title">Settings</h4>
+
+    <div>
+      <input
+        type="checkbox"
+        v-model="notifEnabled"
+        id="notificationCheck"
+        @change="atNotifChange"
+      >
+      <label for="notificationCheck">Enable notification.</label>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AppSettings'
+  name: 'AppSettings',
+
+  data () {
+    return {
+      notifEnabled: localStorage.getItem('notifications')
+    }
+  },
+
+  methods: {
+    atNotifChange () {
+      localStorage.setItem('notifications', this.notifEnabled)
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.app-settings {
+  background-color: var(--second-background);
+  padding: 10px;
+  font-size: 1.6rem;
+
+  &__title {
+    margin-bottom: 8px;
+  }
+}
+</style>
